fabebd7ada073c84926a4016e9a4c12b3a8256d0
Jun Pin Foo <78187667+junpin-foo@users.noreply.github.com>
2024-05-16 19:58:04 -0700

    fix jpg

--- app/Components/Gallery/Gallery.tsx
+++ app/Components/Gallery/Gallery.tsx
@@ -3,9 +3,7 @@ import React from "react";
 import styles from "./Gallery.module.css";
 import CustomImage from "../Images/Images";
 //TODO: dynamically get images from google drive
-import ImageOne from "../../../public/tempImages/Darian1.jpg";
 import ImageTwo from "../../../public/tempImages/Darian2.png";
-import ImageThree from "../../../public/tempImages/Darian3.jpg";
 import ImageFour from "../../../public/tempImages/Darian4.png";
 import ImageFive from "../../../public/tempImages/Darian5.png";
 import ImageSix from "../../../public/tempImages/Darian6.png";
@@ -52,19 +50,6 @@ const Gallery = () => {
                 </motion.div>
                 <motion.div
                     className={styles.imageContainer}
-                    style={{ justifyContent: "flex-end" }}
-                    initial={{x: -200, y: -100}}
-                    whileInView={{x: 0, y: 0}}
-                    transition={{duration: 0.5, delay: 0.25, ease: "easeOut"}}
-                    viewport={{once: true}}
-                >
-                    <CustomImage
-                        imageSource={ImageThree}
-                        imageStyle={{ width: "85%" }}
-                    />
-                </motion.div>
-                <motion.div
-                    className={styles.imageContainer}
                     style={{ justifyContent: "flex-start" }}
                     initial={{x: 200, y: 100}}
                     whileInView={{x: 0, y: 0}}
@@ -72,7 +57,7 @@ const Gallery = () => {
                     viewport={{once: true}}
                 >
                     <CustomImage
-                        imageSource={ImageOne}
+                        imageSource={ImageTwo}
                         imageStyle={{ width: "75%" }}
                     />
                 </motion.div>
