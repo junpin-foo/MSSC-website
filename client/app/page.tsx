e60e7c10743369ac5ff006ca55f9ff83d992487f
thedarianwong <thedarianwong@gmail.com>
2024-05-28 23:24:22 -0700

    Modified Navigation Bar and welcome Section

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,16 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import Gallery from "./Components/Gallery/Gallery";
 import Header from "./Components/Header/Header";
-import Index from "./Components/NavigationBar/NavigationBar";
+import Nav from "./Components/NavigationBar/NavigationBar";
 import Footer from "./Components/Footer/Footer";
+import MainPage from "./Components/MainPage/MainPage"
 
 export default function Home() {
   return (
     <body>
-        <Index></Index>
+
+        <MainPage></MainPage>
+        {/* <Nav></Nav> */}
         {/* <Header></Header> */}
-        <Gallery></Gallery>
-        <Footer></Footer>
+        {/* <Gallery></Gallery>
+        <Footer></Footer> */}
     </body>
   );
 }
